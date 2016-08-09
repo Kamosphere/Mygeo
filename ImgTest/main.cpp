7fcedb68227fb373cccb0677bad2e994362ef9da
Kamosphere <79625970@qq.com>
2016-08-09 17:17:29 +0800

    basicUI

---
+++ ImgTest/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "UIImgTest.h"
+#include <QtWidgets\QApplication>
+#include <QtCore/QTextCodec>
+
+
+int main( int argc, char *argv[] )
+{
+    QApplication a( argc, argv );
+    
+    QTextCodec *codec = QTextCodec::codecForName( "UTF-8" );
+    if( codec == NULL )
+        codec = QTextCodec::codecForLocale();
+        
+    QTextCodec::setCodecForLocale( codec );
+//    QTextCodec::setCodecForTr( codec );
+//    QTextCodec::setCodecForCStrings( codec );
+    
+    UIImgTest w;
+    w.show();
+    return a.exec();
+}
